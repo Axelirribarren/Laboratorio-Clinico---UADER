c70e66083785128a676a5134af352e68537a8801
Axel Irribarren <axelirribarren@gmail.com>
2025-10-29 11:23:03 -0300

    Primera parte

---
+++ app.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const botonDescarga = document.getElementById('btnDescargarPDF');
+
+    botonDescarga.addEventListener('click', () => {
+
+        const elementoParaConvertir = document.querySelector('.informe-pagina');
+
+        const opciones = {
+            margin:       0.2, 
+            filename:     'Informe-Laboratorio-12345.pdf', 
+            image:        { type: 'jpeg', quality: 0.98 }, 
+            html2canvas:  { scale: 2, useCORS: true }, // Escala para mejor resolución
+            jsPDF:        { unit: 'in', format: 'a4', orientation: 'landscape' } // Formato A4
+        };
+
+        // Libreria para crear el pdf
+        html2pdf()
+            .from(elementoParaConvertir)
+            .set(opciones)
+            .save();
+    });
+
+});
